1279f0e423d04a5f3056dedb4d844041c81b216b
friendsmakenoise-prog <friendsmakenoise@googlemail.com>
2026-04-26 23:13:58 +0100

    Update AdminDashboardPage.js

--- frontend/src/pages/AdminDashboardPage.js
+++ frontend/src/pages/AdminDashboardPage.js
@@ -19,6 +19,7 @@ export default function AdminDashboardPage() {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState('')
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => { loadAll() }, [])
 
   async function loadAll() {
