fb4c65b7d61cf072b5da65e8ae4470a30391c001
friendsmakenoise-prog <friendsmakenoise@googlemail.com>
2026-04-26 23:42:50 +0100

    Update SupplierListingsPage.js

--- frontend/src/pages/SupplierListingsPage.js
+++ frontend/src/pages/SupplierListingsPage.js
@@ -29,7 +29,6 @@ export default function SupplierListingsPage() {
   const [brandFilter, setBrandFilter] = useState('')
   const [openToAllOnly, setOpenToAllOnly] = useState(false)
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => { loadAll() }, [])
 
   async function loadAll() {
